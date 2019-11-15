2ba75c84ce83fb5f35d0f92fec4ba2911ab0d092
Yuri Bunduki <yuribunduki@gmail.com>
2019-11-15 21:53:14 +0000

    final changes

--- p_enigma.h
+++ p_enigma.h
@@ -4,7 +4,7 @@
 
 #define MAX_MSG_LENGTH  2000000
 #define ALPHA_LENGTH  26
-#define N_MACHINES_MAX  1
+#define N_MACHINES_MAX  8
 #define PAD             64
 
 void vSetPartitionParams(Params *pParams, size_t iLength);
