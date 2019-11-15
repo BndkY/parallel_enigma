febbfe638e8853acd7a142d0fa7f2beaa3c880ca
Yuri Bunduki <yuribunduki@gmail.com>
2019-11-15 21:32:25 +0000

    Changing max limit

--- p_enigma.h
+++ p_enigma.h
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <omp.h> 
 
-#define MAX_MSG_LENGTH  1500000
+#define MAX_MSG_LENGTH  2000000
 #define ALPHA_LENGTH  26
-#define N_MACHINES_MAX  8
+#define N_MACHINES_MAX  1
 #define PAD             64
 
 void vSetPartitionParams(Params *pParams, size_t iLength);
